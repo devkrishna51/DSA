1105b50ff2a885a409bcbe39469a5989d4758a92
Krishna <ks3259265@gmail.com>
2025-07-29 22:59:42 +0530

    fourth commit

--- basic/GCd.cpp
+++ basic/GCd.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 using namespace std;
-int main()
+int count = 0;
+int print(int a, int b)
 {
-    int a = 12 ;
-    int b = 18 ;
-    for(int i = 1 ; i < a ; i++)
-    {
-        if (a % i == 0 && b % i == 0)
+    count ++;
+        if (a == b)
         {
-            cout << i << "  ";
+            return a ;
         }
-    }
+        else if (a > b)
+        {
+            return print(a-b , b);
+        }
+        return (a,b-a);
+
+}
+int main() {
+    int n = 112122 ;
+    int n2 = 11010101 ;
+   int result = print(n,n2);
+  cout << "the gcd of no is : " << result <<endl;
+  cout << "how many time function call : " << count <<endl;
     return 0;
 }

ec684929fd7f87f1ebe06ca1afd9d78a8aca41f6
Krishna <ks3259265@gmail.com>
2025-08-25 10:52:52 +0530

    25-08-2025

--- array/array III/searchin2Dmatrix.cpp
+++ array/array III/searchin2Dmatrix.cpp
@@ -1,31 +1,48 @@
-#include<iostream>
-using namespace std ; 
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+     bool searchinmatrix(vector<vector<int>> matrix, int target)
+     {
+          int m = matrix.size();
+          int n = matrix[0].size();
+
+          int i = 0;
+          int j = n - 1;
+          while (i < m && j >= 0)
+          {
+               if (matrix[i][j] == target)
+               {
+                    return true;
+               }
+               else if (matrix[i][j] > target)
+               {
+                    j--;
+               }
+               else
+               {
+                    i++;
+               }
+          }
+          return false;
+     }
+};
 int main()
 {
-     int arr[3][3] = {
-          {1,2,3},
-          {4,5,6},
-          {7,8,9},
-     };
-     int target = 5 ; 
-     int i = 0 ; int j = 3-1 ; 
-     while(i < 3 && j>=0)
+     vector<vector<int>> vec = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+     int search;
+     cout << "\n\nEnter a no to search in matrix :  ";
+     cin >> search;
+     Solution s1;
+     bool result = s1.searchinmatrix(vec, search);
+     if(result == true)
      {
-          if(arr[i][j] == target)
-          {
-               cout << target <<"  mila :  " << i << " , " << j <<"  par" ;
-               break ; 
-          }
-          else if(arr[i][j] > target)
-          {
-               j--;
-          }
-          else 
-          {
-               i++ ; 
-          }
-
+          cout <<"\nnumber found in matrix" ; 
      }
-     
-     return 0 ; 
+     else{
+          cout << "\nNumber was not found in matrix" ; 
+     }
+     return 0;
 }
---
+++ array/array III/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+ector <vector<int>> vec = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+     int search ;
+     cout << "Enter a no to search in matrix  ";
+     cin >> search ; 
+     Solution s1(vec,search) ;

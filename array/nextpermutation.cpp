16af27788dde9e4976cf30c0f153ad7396c95646
Krishna <ks3259265@gmail.com>
2025-08-25 17:59:57 +0530

    25-08-25

--- array/nextpermutation.cpp
+++ array/nextpermutation.cpp
@@ -1,42 +1,63 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+class Solution
+{
+    public: 
+    void nextPermutation(vector<int> &arr)
+    {
+        int n = arr.size();
+        int pivot  = -1;
+ 
+    
+        for(int i = n-2 ; i >= 0 ; i--)
+        {
+            if(arr[i] < arr[i+1])
+            {
+                pivot= i ; 
+                break ; 
+            }
+        }
+        if (pivot == -1)
+        {
+            return ; 
+        }
+        for(int i = n-1 ; i >=0 ; i--)
+        {
+            if(arr[i] > arr[pivot])
+            {
+                int temp = arr[i] ; 
+                arr[i] = arr[pivot] ; 
+                arr[pivot] = temp ; 
+                break ; 
+            }
+        }
+        int st = pivot +1 ,end = n-1 ; 
+        while(st < end)
+        {
+            int temp = arr[st] ; 
+            arr[st] = arr[end] ; 
+            arr[end] = temp ; 
+            st++;
+            end-- ; 
+        }
+    }
+};
 int main()
 {
-     int arr[] = {1, 2, 3, 6, 4};
-     int n = sizeof(arr) / sizeof(arr[0]);
-     int pivot = 0;
-     for (int i = n - 2; i >= 0; i--)
-     {
-          if (arr[i] < arr[i + 1])
-          {
-               pivot = i;
-               break;
-          }
-     }   
-     for (int i = n - 1; i >= 0; i--)
-     {
-          if (arr[i] > arr[pivot])
-          {
-               int temp = arr[i];
-               arr[i] = arr[pivot];
-               arr[pivot] = temp;
-               break;
-          }
-     }
- 
-     int start = pivot + 1 , end  = n -1 ; 
-     while(start < end)
-     {
-          int temp = arr[start] ; 
-          arr[start] = arr[end]; 
-          arr[end] = temp ; 
-          start ++ ; 
-          end -- ; 
-     }
-
-     for(int i = 0 ; i < n ; i++)
-     {
-          cout << arr[i] <<"   " ;
-      }
-     return 0;
+    vector<int> vec = {1, 2, 3, 5, 4};
+    for(int i : vec)
+    {
+        cout << i << "  "; 
+    }
+    cout << endl ;
+    Solution s1 ; 
+    s1.nextPermutation(vec) ;
+    cout << endl ;
+       for(int i : vec)
+    {
+        cout << i << "  "; 
+    }
+    cout << endl ;
+    return 0;
 }

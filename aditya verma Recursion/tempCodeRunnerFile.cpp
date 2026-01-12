ac47842afb4b2aeb8126ecef7aa7c9f7d3aa3d1b
Krishna <ks3259265@gmail.com>
2026-01-12 16:11:06 +0530

    Sort a stack in brute force

---
+++ aditya verma Recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+using namespace std ; 
+class Solution
+{
+
+     public : 
+      void sort(stack<int> &s)
+      {
+          int n = s.size( ) ; 
+          vector<int> arr (n) ; 
+          int i = 0 ; 
+          while(i != n)
+          {
+               arr[i] = s.top() ; 
+               s.pop() ; 
+               i++ ; 
+          }
+          sort(arr.begin() , arr.end()) ;  
+          for(int i = 0 ; i < arr.size() ; i++)
+          {
+               s.push(arr[i]) ; 
+          }
+      }
+};
+int main()
+{
+     stack<int> s   ;
+     s.push(2); 
+     s.push(5); 
+     s.push(4); 
+     s.push(3); 
+     s.push(1);
+     Solution s1  ; 
+     s1.sort(s) ; 
+     while(!s.empty())
+     {
+          cout << s.top() <<endl; 
+          s.pop() ; 
+     } 
+     return 0 ; 
+}

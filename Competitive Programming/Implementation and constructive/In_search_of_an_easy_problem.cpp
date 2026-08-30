7adbaa7f94fdd4ff2a7fbe8c9a22bef72ed37000
krishna seth <krishnaseth2425@gmail.com>
2026-08-30 18:52:51 +0530

    In_search_of_an_easy_problem

---
+++ Competitive Programming/Implementation and constructive/In_search_of_an_easy_problem.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+     int n;
+     cin >> n;
+     int count = 0 ; 
+     while(n--){
+          int x ; 
+          cin >> x ; 
+          if(x == 1){
+               count ++ ; 
+          }
+
+     }
+     if(count > 0 ){
+          cout << "HARD" ; 
+     }
+     else{
+          cout << "EASY" ; 
+     }
+     
+     return 0;
+}
